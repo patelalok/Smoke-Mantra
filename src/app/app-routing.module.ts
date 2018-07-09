3b963cec99675bf831e35fe579b5733eac644b36
alokpatel <alokpatel9824107152@gmail.com>
2018-07-08 21:37:14 -0400

    refactored and diveded code in separte module

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,19 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Routes, RouterModule } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+
+
+const routes: Routes = [
+
+  { path: '', component: HomeComponent },
+  // { path: '**', redirectTo: '' },
+];
+@NgModule({
+
+  imports: [
+    CommonModule,
+    RouterModule.forRoot(routes, { useHash: true })],
+    exports: [RouterModule]
+})
+export class AppRoutingModule { }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,15 +1,22 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
 import { AppComponent } from './app.component';
+import { SharedModule } from './shared/shared.module';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { RouterModule } from '@angular/router';
+import { AppRoutingModule } from './app-routing.module';
+import { HomeModule } from './home/home.module';
+
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
   imports: [
-    BrowserModule
+    AppRoutingModule,
+    BrowserModule,
+    SharedModule,
+    HomeModule
   ],
+  exports: [SharedModule],
+  declarations: [AppComponent],
   providers: [],
   bootstrap: [AppComponent]
 })
