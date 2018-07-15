0b247f651e4a9ad26b685bcc490a36227c58bd86
alokpatel <alokpatel9824107152@gmail.com>
2018-07-15 13:13:28 -0400

    some change

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,11 +3,14 @@ import { CommonModule } from '@angular/common';
 import { Routes, RouterModule } from '@angular/router';
 import { HomeComponent } from './home/home.component';
 import { LoginComponent } from './login/login.component';
+import { ProductComponent } from './product/product.component';
 
 
 const routes: Routes = [
   { path: '', component: HomeComponent },
   { path: 'login', component: LoginComponent },
+  { path: 'product', component: ProductComponent },
+
 
 ];
 @NgModule({
